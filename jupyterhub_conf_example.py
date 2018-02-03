d2ad92735b810ee21e893b4f2e6e89c6cee0428a
LE TOULLEC Tristan <tristan.letoullec@cnrs.fr>
2018-02-03 15:48:20 +0100

    Update example configuration

--- jupyterhub_conf_example.py
+++ jupyterhub_conf_example.py
@@ -15,12 +15,20 @@ c.Spawner.http_timeout = 60
 c.Spawner.debug = False
 c.Spawner.notebook_dir = '~/'
 c.Spawner.args = ['']
-c.LibcloudSpawner.cloud_url = "https://KEYSTONE_API/v3/auth/tokens"
+c.LibcloudSpawner.cloud_url = "https://KEYSTONE_API:5000/v3/auth/tokens"
 c.LibcloudSpawner.cloud_user = "jupyter"
 c.LibcloudSpawner.cloud_userpassword = "secret"
 c.LibcloudSpawner.cloud_project = "jupyterproject"
-c.LibcloudSpawner.machine_sizes = [("1vcpu 2Go RAM", "m1.small"), ("4vcpu 8Go RAM", "m1.large")]
-c.LibcloudSpawner.machine_images = [("Default image","jpysingleuser-general-applayer")]
-c.LibcloudSpawner.machine_image = "notebook-machine-template"
-c.LibcloudSpawner.machine_net = "ext-net"
-c.LibcloudSpawner.notebookargs = "--notebook-dir=~"
+c.LibcloudSpawner.machine_sizes = [("1vcpu 2Go RAM", "m1.small"),
+                                   ("4vcpu 8Go RAM", "m1.large")]
+c.LibcloudSpawner.machine_images = [("Default image", "jpysingleuser-general-applayer")]
+c.LibcloudSpawner.machine_net = "my-tenant-network"
+c.LibcloudSpawner.userdata_template_name = "example_userdata_from_vanilla_cloudimage.yaml.j2"
+c.LibcloudSpawner.libcloudparams = {"arg_user_id": "jupyter",
+                                    "arg_key": "secret",
+                                    "ex_force_auth_version": "3.x_password",
+                                    "ex_force_auth_url": "https://KEYSTONE_API:5000/v3/auth/tokens",
+                                    "ex_force_service_region": "RegionOne",
+                                    "ex_tenant_name": "jupyterproject",
+                                    "ex_domain_name": "default",
+                                    "ex_keyname": "tristanlt"}
